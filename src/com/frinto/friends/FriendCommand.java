20b7f3e0ee265b376b488e12456eec75ae3b0f71
Administrator <frinto@riseup.net>
2017-11-22 13:23:25 -0700

    changes

--- src/com/frinto/friends/FriendCommand.java
+++ src/com/frinto/friends/FriendCommand.java
@@ -27,6 +27,7 @@ public class FriendCommand implements CommandExecutor
         
         if(sender instanceof Player)
         {
+<<<<<<< HEAD
 	        if(label.equalsIgnoreCase("friends"))
 	        {
 	            player.sendMessage("/fadd <name>");
@@ -121,6 +122,109 @@ public class FriendCommand implements CommandExecutor
 	        sender.sendMessage("Not a player please try again..");
 	        return false;
 	    }
+=======
+            if (label.equalsIgnoreCase("friends"))
+            {
+                player.sendMessage("/fadd <name>");
+                player.sendMessage("/fremove <name>");
+                player.sendMessage("/flist");
+                return true;
+            } else if (label.equalsIgnoreCase("fadd"))
+            {
+                String requestUUID = player.getUniqueId().toString();
+
+                if (args.length == 0)
+                {
+                    sender.sendMessage(ChatColor.RED + "please specify the player you which to add");
+                    sender.sendMessage(ChatColor.RED + "/fadd <name>");
+                } else
+                {
+                    String targetUUID = null;
+                    
+                    targetUUID = PlayerAPI.getPlayerUUID(args[0]);
+
+
+                    if (targetUUID != null)
+                    {
+                        try
+                        {
+                            
+                            MySQLConnection conn = new MySQLConnection(Main.getMySQLConnectionDetails());
+                            PreparedStatement ps = conn.open().prepareStatement("INSERT INTO Frinto_Friends (requester_uuid, target_uuid, accept_status, time_stamp) VALUES (?,?,?,?);");
+
+                            ps.setString(1, requestUUID);
+                            ps.setString(2, targetUUID);
+                            ps.setInt(3, 0);
+                            Calendar calendar = Calendar.getInstance();
+                            java.sql.Timestamp ourJavaTimestampObject = new java.sql.Timestamp(calendar.getTime().getTime());
+                            ps.setTimestamp(4, ourJavaTimestampObject);
+
+                            conn.doUpdate(ps);
+                        } catch (SQLException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    } else
+                    {
+                        try
+                        {
+                            throw new PlayerHasNotPlayedBefore("player has not played here before exception");
+                        } catch (PlayerHasNotPlayedBefore playerHasNotPlayedBefore)
+                        {
+                            playerHasNotPlayedBefore.printStackTrace();
+                        }
+                    }
+                }
+
+
+                //TESTING WILL REMOVE ALL THIS CODE LATER!
+                try
+                {
+                    MySQLConnection conn2 = new MySQLConnection(Main.getMySQLConnectionDetails());
+                    PreparedStatement ps = conn2.open().prepareStatement("SELECT target_uuid FROM Frinto_Friends WHERE requester_uuid = ?;");
+                    ps.setString(1, requestUUID);
+                    conn2.doQuery(ps, new MySQLConnection.MySQLConnectionBeforeCloseCallback()
+                    {
+                        @Override
+                        public void executeBeforeClose(ResultSet resultSet)
+                        {
+                            try
+                            {
+                                resultSet = ps.executeQuery();
+
+                                while (resultSet.next())
+                                {
+                                    String test2 = resultSet.getString("target_uuid");
+                                    sender.sendMessage(test2);
+                                    int age = 0;
+                                }
+
+                            } catch (SQLException e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } catch (SQLException e)
+                {
+                    e.printStackTrace();
+                }
+
+
+                player.sendMessage(ChatColor.BLUE + "this is for adding friends");
+            } else if (label.equalsIgnoreCase("fremove"))
+            {
+                player.sendMessage(ChatColor.BLUE + "this is for removing friends");
+            } else if (label.equalsIgnoreCase("flist"))
+            {
+                player.sendMessage(ChatColor.BLUE + "this is for viewing friends");
+            }
+        } else
+        {
+            sender.sendMessage("Not a player please try again..");
+            return false;
+        }
+>>>>>>> changes
 
         return false;
     }
